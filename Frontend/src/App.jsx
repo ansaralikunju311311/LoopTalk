8b6f65011b1ce1e98e0c626b706ae72a1c5f3daf
ansaralikunju311311 <ansarpanoor311@gmail.com>
2025-04-07 18:01:34 +0530

    folder setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Landing from './Pages/User/Landing'
+const App = () => {
+  return (
+    <div>
+      <Landing />
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/Pages/User/Landing.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>
+         
+<h1>
+    Talk to strangers,<br/>
+    </h1>
+<p>
+Make friends!
+Experience a random chat alternative to find friends, connect with people, and chat with strangers from all over the world!
+</p>
+
+
+    </div>
+  )
+}
+
+export default Landing
